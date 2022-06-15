fba583d999d270a85327e58828b74b7640cac90d
wwndbs <wwndbs23@gmail.com>
2022-06-15 09:50:12 +0900

    [기초-출력변환] 정수 입력받아 아스키 문자로 출력하기

---
+++ src/ex/Ex_1037.java
@@ -0,0 +1,16 @@
+package ex;
+
+import java.util.Scanner;
+
+public class Ex_1037 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		char ch = (char)x;
+		
+		System.out.println(ch);
+	}
+
+}
