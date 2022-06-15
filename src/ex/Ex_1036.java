761423626a7bf7ae025c59bd25b6e38f1ce27315
wwndbs <wwndbs23@gmail.com>
2022-06-15 09:49:59 +0900

    [기초-출력변환] 영문자 1개 입력받아 10진수로 출력하기

---
+++ src/ex/Ex_1036.java
@@ -0,0 +1,16 @@
+package ex;
+
+import java.util.Scanner;
+
+public class Ex_1036 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char ch = sc.nextLine().charAt(0);
+		int x = (int)ch;
+		
+		System.out.println(x);
+	}
+
+}
