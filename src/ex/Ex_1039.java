5552df1e3e30418ae14fb8144e531627679164a1
wwndbs <wwndbs23@gmail.com>
2022-06-16 11:41:49 +0900

    [기초-산술연산] 정수 2개 입력받아 합 출력하기2

--- src/ex/Ex_1039.java
+++ src/ex/Ex_1039.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class Ex_1039 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    	// 1038번과 동일하여 더 간단한 풀이로 작성
 
+    	Scanner sc = new Scanner(System.in);
 		System.out.println(sc.nextLong() + sc.nextLong());
     }
     
