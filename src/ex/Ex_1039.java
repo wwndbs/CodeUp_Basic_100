872621a1abf1d778a4a52be99a22ac10033716af
wwndbs <wwndbs23@gmail.com>
2022-06-16 11:37:26 +0900

    [기초-산술연산] 정수 2개 입력받아 합 출력하기2(설명)

---
+++ src/ex/Ex_1039.java
@@ -0,0 +1,13 @@
+package ex;
+
+import java.util.Scanner;
+
+public class Ex_1039 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+		System.out.println(sc.nextLong() + sc.nextLong());
+    }
+    
+}
