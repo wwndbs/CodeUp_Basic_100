06eac05269c649e96758619c4ab34b1037048746
wwndbs <wwndbs23@gmail.com>
2022-06-19 22:45:21 +0900

    [기초-산술연산] 문자 1개 입력받아 다음 문자 출력하기

---
+++ src/ex/Ex_1041.java
@@ -0,0 +1,15 @@
+package ex;
+
+import java.util.Scanner;
+
+public class Ex_1041 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char ch = sc.nextLine().charAt(0);
+
+		System.out.printf("%c", ch+1);
+	}
+
+}
