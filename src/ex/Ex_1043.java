d801f99dc59290488a5cc6f2ec87ced58bccdc6a
wwndbs <wwndbs23@gmail.com>
2022-06-21 16:12:46 +0900

    [기초-산술연산] 정수 2개 입력받아 나눈 나머지 출력하기

---
+++ src/ex/Ex_1043.java
@@ -0,0 +1,17 @@
+package ex;
+
+import java.util.Scanner;
+
+public class Ex_1043 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a % b);
+
+	}
+
+}
