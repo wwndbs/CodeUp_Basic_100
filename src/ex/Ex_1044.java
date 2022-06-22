1dad4a80dacc482936d61a5a6d2fdbc8f1bdccde
wwndbs <wwndbs23@gmail.com>
2022-06-22 19:09:06 +0900

    [기초-산술연산] 정수 1개 입력받아 1 더해 출력하기

---
+++ src/ex/Ex_1044.java
@@ -0,0 +1,15 @@
+package ex;
+
+import java.util.Scanner;
+
+public class Ex_1044 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long num = sc.nextLong();
+		
+		System.out.println(++num);
+	}
+
+}
