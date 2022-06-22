73dc73489173b1d903d28c2f72bccaf0574c8324
wwndbs <wwndbs23@gmail.com>
2022-06-22 19:09:16 +0900

    [기초-산술연산] 정수 2개 입력받아 자동 계산하기

---
+++ src/ex/Ex_1045.java
@@ -0,0 +1,23 @@
+package ex;
+
+import java.util.Scanner;
+
+public class Ex_1045 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long a = sc.nextLong();
+		long b = sc.nextLong();
+		
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a % b);
+		
+		float c = (float)a / b;
+		System.out.printf("%.2f", c);
+	}
+
+}
