d3323606dac444e956b4ffc4d3a320293925b72d
wwndbs <wwndbs23@gmail.com>
2022-06-24 17:13:29 +0900

    [기초-비트시프트연산] 정수 1개 입력받아 2배 곱해 출력하기

--- src/ex/Ex_1047.java
+++ src/ex/Ex_1047.java
@@ -6,11 +6,9 @@ public class Ex_1047 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		long num = sc.nextLong();
 		
 		System.out.println(num << 1);
-
 	}
 
 }
