62b64146e4b210b988518ce6d9bf13ded58e3515
wwndbs <wwndbs23@gmail.com>
2022-06-27 21:39:13 +0900

    [기초-비트시프트연산] 한 번에 2의 거듭제곱 배로 출력하기

---
+++ src/ex/Ex_1048.java
@@ -0,0 +1,15 @@
+package ex;
+
+import java.util.Scanner;
+
+public class Ex_1048 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a << b);
+	}
+
+}
