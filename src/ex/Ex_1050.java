f029e3add0c9c8a41c0f875abdea75184001e5b6
wwndbs <wwndbs23@gmail.com>
2022-06-28 23:36:41 +0900

    [기초-비교연산] 두 정수 입력받아 비교하기2

---
+++ src/ex/Ex_1050.java
@@ -0,0 +1,19 @@
+package ex;
+
+import java.util.Scanner;
+
+public class Ex_1050 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if(a == b){
+			System.out.println(1);
+		}else {
+			System.out.println(0);
+		}		
+	}
+
+}
