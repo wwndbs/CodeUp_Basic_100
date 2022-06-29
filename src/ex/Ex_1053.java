8f9bf50ad88a4f0083a6168dfccea84d68414288
wwndbs <wwndbs23@gmail.com>
2022-06-29 22:15:40 +0900

    [기초-논리연산] 참 거짓 바꾸기

---
+++ src/ex/Ex_1053.java
@@ -0,0 +1,18 @@
+package ex;
+
+import java.util.Scanner;
+
+public class Ex_1053 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		
+		if(x == 1){
+			System.out.println(0);
+		}else {
+			System.out.println(1);
+		}		
+	}
+
+}
