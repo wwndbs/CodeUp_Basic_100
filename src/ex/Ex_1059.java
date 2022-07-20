449826a5cf440080629e08ceabf5ce82b549d372
wwndbs <wwndbs23@gmail.com>
2022-07-20 19:08:52 +0900

    [기초-비트단위논리연산] 비트단위로 NOT 하여 출력하기

---
+++ src/ex/Ex_1059.java
@@ -0,0 +1,14 @@
+package ex;
+
+import java.util.Scanner;
+
+public class Ex_1059 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		System.out.println(~a);			
+	}
+
+}
